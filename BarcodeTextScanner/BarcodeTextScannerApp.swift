ba7c5e4b52886d6cd72cdb347847ce8822a5ed08
Pinheiro-dev <mathepba@gmail.com>
2024-04-04 21:22:16 -0300

    Initial Commit

---
+++ BarcodeTextScanner/BarcodeTextScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeTextScannerApp.swift
+//  BarcodeTextScanner
+//
+//  Created by Matheus Pinheiro on 04/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeTextScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
